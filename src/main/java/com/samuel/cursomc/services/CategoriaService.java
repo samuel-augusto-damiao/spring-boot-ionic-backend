4ae7ed32b4fb82f26b458ec6bbd481ae480f4089
Samuel Augusto <samuel_augusto@hotmail.com>
2020-07-27 11:41:39 -0300

    Correcoes: Update em CategoriaService e construtor Pagamento

--- src/main/java/com/samuel/cursomc/services/CategoriaService.java
+++ src/main/java/com/samuel/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.samuel.cursomc.domain.Categoria;
+import com.samuel.cursomc.domain.Cliente;
 import com.samuel.cursomc.dto.CategoriaDTO;
 import com.samuel.cursomc.repositories.CategoriaRepository;
 import com.samuel.cursomc.services.exceptions.DataIntegrityException;
@@ -38,9 +39,10 @@ public class CategoriaService {
 
 	
 	public Categoria update(Categoria obj) {
-	
-		find(obj.getId());
-		return repo.save(obj);
+
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -69,6 +71,11 @@ public class CategoriaService {
 		
 		return new Categoria(objDTO.getId(), objDTO.getNome());
 
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		
+		newObj.setNome(obj.getNome());
 	}
 	
 }
