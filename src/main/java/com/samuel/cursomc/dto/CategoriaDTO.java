e7de324a241f69f2fccf0efdfba0821e61d0b7a1
Samuel Augusto <samuel_augusto@hotmail.com>
2020-07-27 10:39:00 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/samuel/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/samuel/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.samuel.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.samuel.cursomc.domain.Categoria;
 
 
@@ -9,7 +13,11 @@ import com.samuel.cursomc.domain.Categoria;
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	
--- src/main/java/com/samuel/cursomc/services/CategoriaService.java
+++ src/main/java/com/samuel/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.samuel.cursomc.domain.Categoria;
+import com.samuel.cursomc.dto.CategoriaDTO;
 import com.samuel.cursomc.repositories.CategoriaRepository;
 import com.samuel.cursomc.services.exceptions.DataIntegrityException;
 import com.samuel.cursomc.services.exceptions.ObjectNotFoundException;
@@ -63,5 +64,11 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		
+		return new Categoria(objDTO.getId(), objDTO.getNome());
+
+	}
 	
 }
