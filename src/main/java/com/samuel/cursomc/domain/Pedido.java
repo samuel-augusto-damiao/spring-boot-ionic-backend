17eded72b802876253e806ed0178e318305eebad
Samuel Augusto <samuel_augusto@hotmail.com>
2020-07-29 19:54:12 -0300

    Subtotal e Total do Pedido

--- src/main/java/com/samuel/cursomc/domain/Pedido.java
+++ src/main/java/com/samuel/cursomc/domain/Pedido.java
@@ -57,6 +57,18 @@ public class Pedido  implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}	
 	
+
+	public Double getValorTotal() {
+		
+		double soma = 0.0;
+		
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		
+		return soma;
+	}
+	
 	
 	public Integer getId() {
 		return id;
