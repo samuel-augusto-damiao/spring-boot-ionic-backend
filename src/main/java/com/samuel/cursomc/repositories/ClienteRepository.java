4fa7dcc1150f7b4dca7caa6eaa447fc31ea90b91
Samuel Augusto <samuel_augusto@hotmail.com>
2020-07-29 07:51:10 -0300

    Validacao customizada email nao repetido na insercao de cliente

--- src/main/java/com/samuel/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/samuel/cursomc/repositories/ClienteRepository.java
@@ -2,12 +2,15 @@ package com.samuel.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.samuel.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 	
 	
 }
--- src/main/java/com/samuel/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/samuel/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.samuel.cursomc.domain.Cliente;
 import com.samuel.cursomc.domain.enums.TipoCliente;
 import com.samuel.cursomc.dto.ClienteNewDTO;
+import com.samuel.cursomc.repositories.ClienteRepository;
 import com.samuel.cursomc.resources.exceptions.FieldMessage;
 import com.samuel.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -31,6 +39,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido"));
 		}
 
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux!=null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
