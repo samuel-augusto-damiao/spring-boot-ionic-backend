ace693764b5334004f8a1c9eec521fab5f229a0d
Samuel Augusto <samuel_augusto@hotmail.com>
2020-07-29 19:42:16 -0300

    Busca de Produtos por Nome e Categoria

--- src/main/java/com/samuel/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/samuel/cursomc/repositories/ProdutoRepository.java
@@ -1,13 +1,22 @@
 package com.samuel.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.samuel.cursomc.domain.Categoria;
 import com.samuel.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
-	
+	@Transactional(readOnly = true)
+	//@Query("select distinct obj FROM Produto obj inner join obj.categorias cat where obj.nome like %:nome% and cat in :categorias")
+	Page<Produto> findDisctinByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+
 	
 }
