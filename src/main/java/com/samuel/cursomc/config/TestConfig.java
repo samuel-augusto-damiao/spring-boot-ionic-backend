1789eb67ecab5b009c9e7e6bf17867ad11695fce
samuel augusto <samuel_augusto@hotmail.com>
2021-04-05 10:19:07 -0300

    MockEmailService com Logger. Padroes Strategy e template method

--- src/main/java/com/samuel/cursomc/config/TestConfig.java
+++ src/main/java/com/samuel/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.samuel.cursomc.services.DBService;
+import com.samuel.cursomc.services.EmailService;
+import com.samuel.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -23,5 +25,11 @@ public class TestConfig {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
+
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
+	}
+	
 	
 }
